710baa79920d0d653bb70ef084c9930fbf7e3a69
王亚冬 <724743070@qq.com>
2024-05-15 14:47:02 +0800

    fix: 七牛云、腾讯云

--- cloud/tencent.ts
+++ cloud/tencent.ts
@@ -3,7 +3,7 @@
 import { ImageMogr2Opts } from '../types';
 
 /**
- * https://developer.qiniu.com/dora/1279/basic-processing-images-imageview2
+ * https://cloud.tencent.com/document/api/1246/45375
  * 图片缩略 + 渐进显示 + 格式转换
  * @param {string} url
  * @param {"w" | "h" | "*"} type
@@ -12,13 +12,19 @@ import { ImageMogr2Opts } from '../types';
  * @param {boolean} interlace
  * @returns {string}
  */
-export const imageMogr2 = ({ url, type = '*', size, format, interlace = true }: ImageMogr2Opts) => {
+export const imageMogr2 = ({
+    url,
+    type = '*',
+    size,
+    format = 'png',
+    interlace = true
+}: ImageMogr2Opts) => {
     const [w, h] = Array.isArray(size) ? size : [size, size];
     return `${url}?imageMogr2/thumbnail/${
         type === 'w' ? `${w}x`
         : type === 'h' ? `x${w}`
         : `${w}x${h}`
-    }${!format ? '' : `/format/${format}`}/interlace/${Number(interlace)}`;
+    }/format/${format}/interlace/${Number(interlace)}`;
 };
 /**
  * 获取图片基本信息
